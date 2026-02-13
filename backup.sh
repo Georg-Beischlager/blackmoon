48f8724781fc7313bf1bea5d02e9aef2cfd058c2
Georg Beischlager <georg.beischlager@proton.me>
2026-02-13 19:48:10 +0100

    - improved backup script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 printf -v date '%(%Y%m%d_%H%M%S)T\n' -1
 now=$(date +%F_%H-%M-%S)
 echo "now: ${now}"
-zip -r /mnt/HC_Volume_102781745/blackmoon/blackmoon_${now}.zip /node/blackmoon/data /node/blackmoon/media
+zip -r /mnt/HC_Volume_102781745/blackmoon/blackmoon_${now}.zip /node/blackmoon/data /node/blackmoon/media /node/blackmoon/hex-images
 echo "Backup complete: /mnt/HC_Volume_102781745/blackmoon/blackmoon_${now}.zip"
